9edeb53f9087084ea2c8caf9bf023c1cd24fe1c2
ehinson <erinqhinson@gmail.com>
2019-06-05 18:02:31 -0700

    sync and async validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import React, { Component} from "react";
-import { hot } from "react-hot-loader";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-import Form from '../src/views/components/Register';
+import RegisterForm from './views/components/Register';
 
 function Index() {
   return <h2>Home</h2>;
@@ -17,36 +16,32 @@ function Users() {
   return <h2>Users</h2>;
 }
 
-class App extends Component{
-  render(){
-    return(
-      <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login/">login</Link>
-            </li>
-            <li>
-              <Link to="/users/">Users</Link>
-            </li>
-            <li>
-              <Link to="/register/">Register</Link>
-            </li>
-          </ul>
-        </nav>
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login/">login</Link>
+          </li>
+          <li>
+            <Link to="/users/">Users</Link>
+          </li>
+          <li>
+            <Link to="/register/">Register</Link>
+          </li>
+        </ul>
+      </nav>
 
-        <Route path="/" exact component={Index} />
-        <Route path="/login/" component={About} />
-        <Route path="/users/" component={Users} />
-        <Route path="/register/" component={Form} />
-      </div>
-    </Router>
-    );
-  }
-}
+      <Route path="/" exact component={Index} />
+      <Route path="/login/" component={About} />
+      <Route path="/users/" component={Users} />
+      <Route path="/register/" component={RegisterForm} />
+    </div>
+  </Router>
+);
 
 export default hot(module)(App);
--- client/src/views/components/Register.js
+++ client/src/views/components/Register.js
@@ -1,21 +1,59 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
-const Basic = () => (
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .ensure()
+    .required(),
+  email: yup
+    .string()
+    .ensure()
+    .email()
+    .required(),
+  password: yup
+    .string()
+    .ensure()
+    .required()
+    .test('password', 'passwords must match', function(value) {
+      const { password__confirmation } = this.parent;
+      return value === password__confirmation;
+    }),
+  password__confirmation: yup
+    .string()
+    .ensure()
+    .required()
+    .test('password__confirmation', 'passwords must match', function(value) {
+      const { password } = this.parent;
+      return value === password;
+    })
+});
+
+// Async Validation
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const validate = (values, props /* only available when using withFormik */) => {
+  return sleep(1000).then(() => {
+    let errors = {};
+    if (['admin', 'null', 'god'].includes(values.username)) {
+      errors.username = 'Nice try';
+    }
+    // ...
+    if (Object.keys(errors).length) {
+      throw errors;
+    }
+  });
+};
+
+const RegisterForm = () => (
   <div>
     <h1>Any place in your app!</h1>
     <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
+      initialValues={{ username: '', email: '', password: '', password__confirmation: '' }}
+      validate={validate}
+      validationSchema={validationSchema}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
@@ -24,18 +62,28 @@ const Basic = () => (
       }}
     >
       {({ isSubmitting }) => (
-        <Form>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="div" />
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
+        <Fragment>
+          <Form>
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="div" />
+
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+
+            <Field type="password" name="password__confirmation" />
+            <ErrorMessage name="password__confirmation" component="div" />
+
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        </Fragment>
       )}
     </Formik>
   </div>
 );
 
-export default Basic;
+export default RegisterForm;
